6ad832ceb186c530bd75030daba658d112644fd7
TatsuhiroFuruta <40419629+TatsuhiroFuruta@users.noreply.github.com>
2026-03-19 17:42:37 +0900

    闇の時間の活動内容の情報を取得

--- app/controllers/activity_records_controller.rb
+++ app/controllers/activity_records_controller.rb
@@ -7,6 +7,7 @@ class ActivityRecordsController < ApplicationController
     # タスク内容を登録のためこちらでインスタンス化
     @activity_record = current_user.activity_records.build(task: params[:task])
     @light_time = current_user.light_times.find_by(is_current: true)
+    @dark_time = current_user.dark_time
   end
 
   private
