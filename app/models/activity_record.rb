d117d20640b9f5406bb23b0e623bdd9cfaa1295f
TatsuhiroFuruta <40419629+TatsuhiroFuruta@users.noreply.github.com>
2026-03-25 14:24:44 +0900

    今日の本来の自分の算出処理記述

--- app/models/activity_record.rb
+++ app/models/activity_record.rb
@@ -2,4 +2,12 @@ class ActivityRecord < ApplicationRecord
   belongs_to :user
   belongs_to :light_time
 
+  before_save :calculate_desired_self_percentage
+
+  def calculate_desired_self_percentage
+    return if total_duration.to_i == 0
+
+    self.desired_self_percentage =
+      (total_duration - idle_duration).to_f / total_duration
+  end
 end
