c63e625be0a080c490fa382ca82214a80e5c837b
TatsuhiroFuruta <40419629+TatsuhiroFuruta@users.noreply.github.com>
2026-03-25 15:49:53 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
     end
   end
   # 光の時間の活動記録
-  resources :activity_records, only: %i[index new] do
+  resources :activity_records, only: %i[index new create] do
     collection do
       get :pomodoro_timer    # タイマーページ
     end
