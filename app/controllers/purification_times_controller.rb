e2001c7c1b1f1c389265f6c4469952587c2b3565
TatsuhiroFuruta <40419629+TatsuhiroFuruta@users.noreply.github.com>
2026-04-06 22:57:55 +0900

    浄化タイマー実行中にリセットボタンが表示されない＆処理が実行されないことを確認

--- app/controllers/purification_times_controller.rb
+++ app/controllers/purification_times_controller.rb
@@ -15,8 +15,12 @@ class PurificationTimesController < ApplicationController
   end
 
   def reset
-    @purification_time.reset!
-    head :ok
+    unless @purification_time.running?
+      @purification_time.reset!
+      head :ok
+    else
+      redirect_to mypage_path, alert: "タイマー実行中です"
+    end
   end
 
   private
