7881b707d906a3069a520577b47277a962769def
TatsuhiroFuruta <40419629+TatsuhiroFuruta@users.noreply.github.com>
2026-04-08 01:56:27 +0900

    ログアウトしましたが表示されるようにメソッドを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,11 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  # 「ログアウトしました。」のフラッシュメッセージ表示できたことを確認済み
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path  # ログインページにリダイレクト
+  end
+
   def configure_permitted_parameters
 		# 名前を新規登録する場合に必要
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
