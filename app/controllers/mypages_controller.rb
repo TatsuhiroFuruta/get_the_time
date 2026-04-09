7cfc0d6b988f6b73cac9acabfdf8972e5a2fe125
TatsuhiroFuruta <40419629+TatsuhiroFuruta@users.noreply.github.com>
2026-04-09 15:50:59 +0900

    今日の光の時間の表示&浄化タイマーの時間を表示するヘルパー関数名の修正

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -5,5 +5,6 @@ class MypagesController < ApplicationController
     @dark_time = current_user.dark_time
     @light_time = current_user.light_times.find_by(is_current: true) || current_user.light_times.first
     @purification_time = current_user.purification_time
+    @today_light_time = ActivityRecord.total_light_time_today(current_user)
   end
 end
