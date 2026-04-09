4aa8d47d57e6e47fae81e050212d7143eebb3525
TatsuhiroFuruta <40419629+TatsuhiroFuruta@users.noreply.github.com>
2026-04-09 14:33:22 +0900

    不要なコードを削除

--- app/controllers/activity_records_controller.rb
+++ app/controllers/activity_records_controller.rb
@@ -4,7 +4,6 @@ class ActivityRecordsController < ApplicationController
   before_action :set_activity_record, only: %i[show edit update destroy]
 
   def index
-    # @activity_records = current_user.activity_records.includes(:light_time).order(created_at: :desc)
     @q = current_user.activity_records.ransack(params[:q])
     @activity_records = @q.result(distinct: true).includes(:light_time).order(created_at: :desc).page(params[:page]).per(8)
   end
