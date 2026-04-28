3fc4ef7ba03edc78ab74c168011ed2dccccbf498
TatsuhiroFuruta <40419629+TatsuhiroFuruta@users.noreply.github.com>
2026-04-29 01:02:51 +0900

    how_to_useアクションをログイン後のみ有効

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:home]
   def home; end
 
   def how_to_use; end
