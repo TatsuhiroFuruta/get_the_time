9d077813bf0e008a336721fb70a285799e215bbc
TatsuhiroFuruta <40419629+TatsuhiroFuruta@users.noreply.github.com>
2026-05-11 23:40:29 +0900

    ダブルクォーテーションに変更

--- spec/models/light_time_spec.rb
+++ spec/models/light_time_spec.rb
@@ -1,69 +1,69 @@
 require 'rails_helper'
 
 RSpec.describe LightTime, type: :model do
-  describe 'アソシエーション' do
-    it 'Userに属していること' do
+  describe "アソシエーション" do
+    it "Userに属していること" do
       association = described_class.reflect_on_association(:user)
       expect(association.macro).to eq :belongs_to
     end
   end
 
-  describe 'バリデーション' do
-    it 'factory は有効であること' do
+  describe "バリデーション" do
+    it "factory は有効であること" do
       expect(build(:light_time)).to be_valid
     end
 
-    it 'action のみでも有効であること' do
+    it "action のみでも有効であること" do
       light_time = build(:light_time, characteristic: nil, desired_self: nil)
       expect(light_time).to be_valid
     end
 
-    it 'action が nil の場合は無効であること' do
+    it "action が nil の場合は無効であること" do
       light_time = build(:light_time, action: nil)
       expect(light_time).to be_invalid
       expect(light_time.errors[:action]).to be_present
     end
 
-    it 'user が nil の場合は無効であること' do
+    it "user が nil の場合は無効であること" do
       light_time =build(:light_time, user: nil)
       expect(light_time).to be_invalid
       expect(light_time.errors[:user]).to be_present
     end
   end
 
-  describe 'デフォルト値' do
-    it 'is_current の初期値は false であること' do
+  describe "デフォルト値" do
+    it "is_current の初期値は false であること" do
       light_time = create(:light_time)
       expect(light_time.is_current).to be false
     end
   end
 
-  describe '.switch_current!' do
+  describe ".switch_current!" do
     let(:user) { create(:user) }
 
     let!(:current_light_time) { create(:light_time, :current, user: user) }
     let!(:next_light_time) { create(:light_time, user: user) }
     let!(:third_light_time) { create(:light_time, user: user) }
 
-    context '正常系' do
-      it '指定した light_time が current になること' do
+    context "正常系" do
+      it "指定した light_time が current になること" do
         described_class.switch_current!(user, next_light_time)
         expect(next_light_time.reload.is_current).to be true
       end
 
-      it '他の light_time は current ではなくなること' do
+      it "他の light_time は current ではなくなること" do
         described_class.switch_current!(user, next_light_time)
         expect(current_light_time.reload.is_current).to be false
         expect(third_light_time.reload.is_current).to be false
       end
 
-      it 'current は1件だけになること' do
+      it "current は1件だけになること" do
         described_class.switch_current!(user, next_light_time)
 
         expect(user.light_times.where(is_current: true).count).to eq 1
       end
 
-      it '他ユーザーには影響しないこと' do
+      it "他ユーザーには影響しないこと" do
         other_user = create(:user)
         other_light_time = create(:light_time, :current, user: other_user)
         described_class.switch_current!(user, next_light_time)
@@ -71,8 +71,8 @@ RSpec.describe LightTime, type: :model do
       end
     end
 
-    context '異常系' do
-      it 'update! 失敗時は rollback されること' do
+    context "異常系" do
+      it "update! 失敗時は rollback されること" do
         allow(next_light_time).to receive(:update!).and_raise(ActiveRecord::RecordInvalid)
 
         expect {described_class.switch_current!(user, next_light_time)
