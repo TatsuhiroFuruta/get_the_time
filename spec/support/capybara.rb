4fe43016f953a7e2b166448dc994009af2fd95cc
TatsuhiroFuruta <40419629+TatsuhiroFuruta@users.noreply.github.com>
2026-05-21 22:47:52 +0900

    CI: minitestからRSpecへ実行を切り替え
    - ci.yml: test/system-testジョブを削除し、RSpec実行のrspecジョブに統合
    - ci.yml: system spec実行のためのJS/CSSビルドステップを追加
    - ci.yml: bundler-audit実行前にDB更新を追加
    - capybara.rb: CI環境用のheadless_chromeドライバを追加
    - capybara.rb: 環境変数SELENIUM_DRIVER_URLの有無で自動的にドライバ切替

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,3 +1,7 @@
+require "capybara/rspec"
+require "selenium-webdriver"
+
+# Docker開発環境用(リモートChromeコンテナを使用)
 Capybara.register_driver :remote_chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new
   options.add_argument("no-sandbox")
@@ -10,4 +14,24 @@ Capybara.register_driver :remote_chrome do |app|
     url: ENV["SELENIUM_DRIVER_URL"],
     capabilities: options
   )
+end
+
+# CI環境/ローカル直接実行用(ホストのHeadless Chromeを使用)
+Capybara.register_driver :headless_chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument("--headless=new")
+  options.add_argument("--no-sandbox")
+  options.add_argument("--disable-gpu")
+  options.add_argument("--window-size=1680,1050")
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    if ENV["SELENIUM_DRIVER_URL"].present?
+      driven_by :remote_chrome
+    else
+      driven_by :headless_chrome
+    end
+  end
 end
